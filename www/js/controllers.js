be2396d14eb5b7ed3a69c4ce635fbff2184cc2df
Nadiar AS <codxse@gmail.com>
2016-04-28 11:01:13 +0700

    getMetaData() function done without error

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -156,14 +156,28 @@ angular.module('ionJakarta.controllers', [])
     };
 })
 
-.controller('PageChartLineCtrl', ['$scope', '$stateParams',
-  function($scope, $stateParams) {
+.controller('PageChartLineCtrl', ['$scope', '$stateParams', 'metaDataServices',
+  function($scope, $stateParams, metaDataServices) {
     $scope.ticker = {
-      tag: $stateParams.tagTicker,
+      sref: $stateParams.srefTicker,
       name: $stateParams.nameTicker,
-      sref: $stateParams.srefTicker
+      tag: $stateParams.tagTicker,
+      chart: $stateParams.chartTicker
     };
-
+    console.log('in PageChartLineCtrl: ');
     console.log($scope.ticker);
+
+    // When Ionic catching the event, function on $scope.on will be invoked
+    $scope.$on("$ionicView.afterEnter", function() {
+      getMetaData();
+    });
+
+    function getMetaData() {
+      var promise = metaDataServices.getMetaData($scope.ticker);
+      promise.then(function(jsonData) {
+        console.log('in PageChartLineCtrl: promise function');
+        console.log(jsonData);
+      });
+    }
   }
 ]);
--- www/js/services.js
+++ www/js/services.js
@@ -1,4 +1,30 @@
 angular.module('ionJakarta.services', [])
 
+.factory('metaDataServices', function($q, $http) {
 
-;
+  var deferred = $q.defer(),
+  url = "http://finance.yahoo.com/webservice/v1/symbols/" + "YHOO" + "/quote?format=json&view=detail";
+
+  var getMetaData = function(ticker) {
+    $http.get(url)
+    .success(function(jsonData) {
+      console.log('in getMetaData: ');
+      console.log(ticker);
+      console.log('in getMetaData: Full jsonData');
+      console.log(jsonData);
+      console.log('in getMetaData: jsonData.list.resources[0].resource.fields');
+      var jsonData = jsonData.list.resources[0].resource.fields;
+      console.log(jsonData);
+      deferred.resolve(jsonData);
+    })
+    .error(function(error) {
+      console.log("metaDataServices error: " + error);
+      deferred.reject();
+    });
+    return deferred.promise;
+  };
+
+  return {
+    getMetaData: getMetaData
+  }
+});
